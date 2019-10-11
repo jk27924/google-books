fcfdeaf16f346a0819621e83f80827671b8d9bc4
Jihong Kim <jk279242naver.com>
2019-10-10 19:07:35 -0500

    created gitignore, and deleted all components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import Navbar from "./components/Nav/Nav";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Jumbotron from "./components/Jumbotron/Jumbotron";
-import HomePage from "./components/Pages/HomePage";
-import SavedArticle from "./components/Pages/SavedArticle";
+
 
 
 
@@ -12,14 +8,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
           <div className="App-header">
-            <Navbar />
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/SavedArticle" component={SavedArticle} />
             <h2>Welcome to React</h2>
           </div>
-        </Router>
       </div>
     );
   }
