98f7a91c24c2a68a33cd31e207cb837dda5183f7
Jihong Kim <jk279242naver.com>
2019-10-15 14:21:01 -0500

    adding routes in process.

---
+++ routes/api/index.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const apiRoutes = require ("./searchAPI");
+
+// router.use("./books", apiRoutes);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
 const axios = require("axios");
+const routes = require("./routes");
 
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Add routes
+app.use(routes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -21,9 +25,9 @@ mongoose.connect("mongodb://localhost/googlebooks", { useNewUrlParser: true });
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  // });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
